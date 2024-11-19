e955e22e825ffada3b9a217d758dc3eeb22d2572
Ram <91278686+Ramlakhan12@users.noreply.github.com>
2024-11-20 02:03:12 +0530

    Delete recommender directory

--- recommender/static/recommender/script.js
+++
@@ -1,8 +0,0 @@
-function search() {
-    let query = document.querySelector('.search-bar input').value;
-    if (query) {
-        alert(`Searching for "${query}"...`);
-    } else {
-        alert("Please enter a search term!");
-    }
-}
--- recommender/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-
-from . import views
-
-urlpatterns = [
-    path('',views.top_restaurants_view, name="forst")
-]
